356ef2e232a7cd9fd65cdedf016b33ec4e7a3b8c
yuki-morita-dayo <yuki.morita@leadingmark.jp>
2025-04-15 11:58:49 +0900

    kondokoso good bye

--- src/pages/api/slack.ts
+++ src/pages/api/slack.ts
@@ -1,11 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_ANON_KEY!;
-const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL!;
-const supabase = createClient(supabaseUrl, supabaseKey);
 
 type Data = {
   message: string;
